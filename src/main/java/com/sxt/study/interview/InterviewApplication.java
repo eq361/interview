85fd3504d3d278be13d1b4a46b679daadf1b6a01
songjun <1906409331@qq.com>
2019-07-23 08:52:39 +0800

    Initial commit

---
+++ src/main/java/com/sxt/study/interview/InterviewApplication.java
@@ -0,0 +1,16 @@
+package com.sxt.study.interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author songjun
+ */
+@SpringBootApplication
+public class InterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewApplication.class, args);
+	}
+
+}
